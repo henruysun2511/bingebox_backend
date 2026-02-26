630a6d750aa2cbe62d8d7a3cc5337fbe0ceeb40b
huysun <huysun2511@gmail.com>
2026-02-26 16:04:51 +0700

    fix(seat): fix seat validation

--- src/utils/calcAge.ts
+++ src/utils/calcAge.ts
@@ -1,11 +1,17 @@
-export function calcAge(birth?: Date): number {
+export function calcAge(birth?: Date | string): number {
   if (!birth) return 0;
 
-  const today = new Date();
-  let age = today.getFullYear() - birth.getFullYear();
-  const m = today.getMonth() - birth.getMonth();
+  // Ép kiểu về Date để đảm bảo có các hàm getFullYear, getMonth...
+  const birthDate = new Date(birth);
+  
+  // Kiểm tra nếu date không hợp lệ (Invalid Date)
+  if (isNaN(birthDate.getTime())) return 0;
 
-  if (m < 0 || (m === 0 && today.getDate() < birth.getDate())) {
+  const today = new Date();
+  let age = today.getFullYear() - birthDate.getFullYear();
+  const m = today.getMonth() - birthDate.getMonth();
+
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
     age--;
   }
   return age;
