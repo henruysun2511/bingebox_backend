391b03b2e8f56e58e760f62665832cbd521c3593
huysun <huysun2511@gmail.com>
2026-02-27 23:00:32 +0700

    final

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import http from "http";
+import passport from "passport"; // 👈 THÊM DÒNG NÀY
+import "./config/passport";
 import { connectDB } from "./configs/connectDB";
 import { initIo } from "./configs/socket.config";
 import { startReleaseSeatCron } from "./crons/releaseSeat.cron";
@@ -15,32 +17,35 @@ dotenv.config();
 
 const app = express();
 const PORT = ENV.PORT;
+
 app.use(express.json());
 app.use(cookieParser());
-const allowedOrigins = [
-  process.env.CLIENT_URL, 
-  process.env.CLIENT_URL_2
-].filter((origin): origin is string => Boolean(origin)); 
 
-app.use(cors({
-  origin: allowedOrigins,
-  credentials: true
-}));
+app.use(passport.initialize());
+
+const allowedOrigins = [
+  process.env.CLIENT_URL,
+  process.env.CLIENT_URL_2,
+].filter((origin): origin is string => Boolean(origin));
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
 
 app.use("/api/v1", routes);
-
 app.use(errorHandlerMiddleware);
 
 const server = http.createServer(app);
 const io = initIo(server);
 registerSeatSocket(io);
 
-
 connectDB().then(() => {
   startReleaseSeatCron();
 
-  app.listen(PORT, () => {
+  server.listen(PORT, () => {  
     console.log(`server bắt đầu trên cổng ${PORT}`);
   });
 });
-
